cc7cbc571bc2cb07baad1fa8b21e8b158ce11b87
Prerna <125128710+PrernaMi@users.noreply.github.com>
2023-11-04 07:18:42 +0530

    Add files via upload

--- SQL_Learn/Temp.sql
+++ SQL_Learn/Temp.sql
@@ -13,6 +13,9 @@ values
 (2,'Soniya','Hindi'),
 (3,'Kamni','Hindi');
 
+alter table student
+add column course varchar(30);
+
 select * from student;
 select now();
 select upper('Prerna');
@@ -23,8 +26,8 @@ where name='Prerna';
 
 select name from student where id>=2;
 
-select * from student 
-order by name asc;
+select * from student; 
+
 
 select subject from student
 where id between 1 and 3;
@@ -41,7 +44,9 @@ select name from student where subject is null;
 select name from student where name like '_a%';
 
 select name from student order by name asc;
+
 select distinct id from student;
+
 select name from student where subject='Hindi' order by name limit 2;
 
 alter table student
@@ -75,5 +80,23 @@ select name , id from student where marks >= 567 order by name limit 3;
 select count(subject) from student group by subject;
 select subject,max(marks) from student group by subject;
 
-select name ,min(marks) as Marks from student group by name having id between 1 and 4 limit 2;
-select subject,count(*) as Total from student group by subject;
+select id ,min(marks) as Marks from student group by id having id between 1 and 4 limit 2;
+
+select subject,count(*) as Total from student group by subject having Total >= 2;
+
+select name from student where name like 'p%';
+
+set SQL_safe_updates =0;
+
+update student set course ='CSE';
+
+update student set 
+course = 'IT',name='Rahul'
+where id = 1;
+
+alter table student
+add column age int;
+
+update student
+set
+age = 24;
